80b2557bad8b9fa520c276df93c413c8112be64a
Jonas Bandi <dev@jonasbandi.net>
2018-01-15 15:32:01 +0100

    Solution exercise 3

---
+++ 01-Starter/fantastic-ng-solution/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
